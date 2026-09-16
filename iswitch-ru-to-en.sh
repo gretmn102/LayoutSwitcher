b592855f8b9869b4c72fd1c9db0b217c1d3e5b5d
gretmn102 <erlgreywatson@gmail.com>
2026-09-17 02:59:30 +0300

    feat(iswitch-ru-to-en): add copy to clipboard

--- iswitch-ru-to-en.sh
+++ iswitch-ru-to-en.sh
@@ -7,17 +7,24 @@ usage() {
   echo 'Программа создает временный файл и открывает его в `nano`. По выходу всё содержимое преобразовывается через `switch-ru-to-en` и выводится в консоль. Временный файл уничтожается.'
   echo
   echo "Where:"
+  echo "-c            To clipboard"
   echo
   echo "Options:"
   echo "  --help, -h  Show this help."
   exit 1
 }
 
+to_clipboard=false
+
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -h|--help)
       usage
       exit 0
+      ;;
+    -c)
+      to_clipboard=true
+      shift
       ;;
     -*)
       echo "Error: Unknown option: $1" >&2
@@ -37,4 +44,11 @@ nano "$temp_file"
 content=$(<"$temp_file")
 rm "$temp_file"
 
-switch-ru-to-en "$content"
+result=$(switch-ru-to-en "$content")
+
+if [ "$to_clipboard" = true ]; then
+  termux-clipboard-set "$result"
+  echo "Copied output to clipboard."
+else
+  echo "$result"
+fi
